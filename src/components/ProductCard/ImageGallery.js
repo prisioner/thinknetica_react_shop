f86943cf63fc80b6cafe727316a85109f6a0e668
Aleksandr Klimenkov <prisioner@mail.ru>
2019-09-28 18:29:27 +0300

    add lightbox

--- src/components/ProductCard/ImageGallery.js
+++ src/components/ProductCard/ImageGallery.js
@@ -1,6 +1,8 @@
-import React from "react"
+import React, { Fragment } from "react"
 import PropTypes from "prop-types"
 import { Carousel, Image } from "react-bootstrap"
+import Lightbox from "react-image-lightbox"
+import "react-image-lightbox/style.css"
 
 export default class ImageGallery extends React.PureComponent {
   static propTypes = {
@@ -20,20 +22,42 @@ export default class ImageGallery extends React.PureComponent {
 
   render() {
     const { images, alt } = this.props
+    const { isOpen, photoIndex } = this.state
 
     const items = images.map((item, index) => (
       <Carousel.Item key={index}>
         <Image
           src={item} alt={`[${alt}]`}
+          onClick={() => this.setState({ isOpen: true, photoIndex: index })}
           fluid
         />
       </Carousel.Item>
     ))
 
     return(
-      <Carousel>
-        {items}
-      </Carousel>
+      <Fragment>
+        <Carousel>
+          {items}
+        </Carousel>
+        {isOpen && (
+          <Lightbox
+            mainSrc={images[photoIndex]}
+            nextSrc={images[(photoIndex + 1) % images.length]}
+            prevSrc={images[(photoIndex + images.length - 1) % images.length]}
+            onCloseRequest={() => this.setState({ isOpen: false })}
+            onMovePrevRequest={() =>
+              this.setState({
+                photoIndex: (photoIndex + images.length - 1) % images.length,
+              })
+            }
+            onMoveNextRequest={() =>
+              this.setState({
+                photoIndex: (photoIndex + 1) % images.length,
+              })
+            }
+          />
+        )}
+      </Fragment>
     )
   }
 }
